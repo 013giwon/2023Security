09eda5cf402a2780f643a2b4c8720e5f65e3010c
giwonsur <013giwon@gmail.com>
2023-05-23 22:51:09 +0900

    tcp aes communication

--- client.py
+++ client.py
@@ -6,14 +6,14 @@ from aes import AES
 import numpy as np
 import logging
 import pdb
-from patches import patches
+# from patches import patches
 
-UDP_IP = '127.0.0.1'
-UDP_PORT = 9505
+TCP_IP = '127.0.0.1'
+TCP_PORT = 9505
 mode ="ecb"
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)#socket.SOCK_DGRAM)
 #UDP인경우 생략, TCP는 해주어야 함
-sock.connect((UDP_IP, UDP_PORT))
+sock.connect((TCP_IP, TCP_PORT))
 
 
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
@@ -21,6 +21,7 @@ cap = cv2.VideoCapture(0)
 org_file = 'Jungkook.png'
 img = cv2.imread(org_file)
 aes = AES("aes", 256)
+iteration = 1
 start1 = time.time()
 
 aes.key_generation()
@@ -36,58 +37,82 @@ def trans_format_RGB(data):
 while True:
     # ret, frame = cap.read()
     time.sleep(2)
-    # faces = face_cascade.detectMultiScale(img, 1.3, 5)
-    # for (x,y,w,h) in faces:
-    #     img = cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
-    #     roi_color = img[y:y+h, x:x+w]
-    #     cv2.imwrite("target.jpg", roi_color)
-    #     print("image size {}".format(img.size))
-    #     filename = "target.jpg"
-    #     im = Image.open(filename)
-        # roi_color = Image.fromarray(numpy.uint8(roi_color))    
-    # value_vector = roi_color.convert("RGB").tobytes()
+
     filename = "target.jpg"
     im = Image.open(filename)
-
-
     
     value_vector = im.convert("RGB").tobytes()
+    print(len(value_vector))
     imlength = len(value_vector)
-    # if mode == "ecb":
-    #     start2 = time.time()
-    #     encrypted_vector = aes.aes_ecb_encrypt(aes.pad(value_vector))
-    #     end2 = time.time()
-    # elif mode == "cbc":
-    #     start2 = time.time()
-    #     encrypted_vector = aes.aes_cbc_encrypt(aes.pad(value_vector))
-    #     end2 = time.time()
-    # elif mode == "ofb":
-    #     start2 = time.time()
-    #     encrypted_vector = aes.aes_ofb_encrypt(aes.pad(value_vector))
-    #     end2 = time.time()
-
-    # print("key enc {} s".format(end2-start2))
-    # # 
+    # pdb.set_trace()
+ #-------------------------------
+    if mode == "ecb":
+        start2 = time.time()
+        encrypted_vector = aes.aes_ecb_encrypt(aes.pad(value_vector))
+        end2 = time.time()
+    elif mode == "cbc":
+        start2 = time.time()
+        encrypted_vector = aes.aes_cbc_encrypt(aes.pad(value_vector))
+        end2 = time.time()
+    elif mode == "ofb":
+        start2 = time.time()
+        encrypted_vector = aes.aes_ofb_encrypt(aes.pad(value_vector))
+        end2 = time.time()
     # value_encrypt = trans_format_RGB(encrypted_vector[:imlength])
-    value_vector = trans_format_RGB(value_vector[:imlength])
-    # logging.info("value_encrypt: {}".format(value_encrypt))
-    # im2 = Image.new(im.mode, im.size)
-    # im2.putdata(value_encrypt)
-    # value_encrypt = np.array(value_encrypt)
-    value_vector = np.array(value_vector)
-    # d = value_encrypt.flatten()
-    d = value_vector.flatten()
+    value_encrypt = np.array(encrypted_vector)
+    d = value_encrypt.flatten()
     s = d.tostring()
+    pdb.set_trace()
+    # ssss = np.fromstring(s, dtype=np.uint8)
+    # im = Image.new(mode="RGB", size=(int(np.sqrt(len(ssss)/3)),int(np.sqrt(len(ssss)/3))))
+    # im.putdata(ssss)
+    encrypted_vector = s #im.convert("RGB").tobytes()
+    if mode == "ecb":
+        start3 = time.time()
+        for i in range(iteration):
+            decrypted_vector = aes.aes_ecb_decrypt(aes.pad(encrypted_vector))
+        end3 = time.time()
+    elif mode == "cbc":
+        start3 = time.time()
+        for i in range(iteration):
+            decrypted_vector = aes.aes_cbc_decrypt(encrypted_vector)
+        end3 = time.time()
+    elif mode == "ofb":
+        start3 = time.time()
+        for i in range(iteration):
+            decrypted_vector = aes.aes_ofb_decrypt(encrypted_vector)
+        end3 = time.time()
+    just_vector = decrypted_vector
+ #-------------------------------
+    ##value_vector = np.array(value_vector)
+    ##d = value_vector.flatten()
+    
+   
 
     for i in range(20):
-        sock.sendto(bytes([i]) + s[i * 19080:(i + 1) * 19080], (UDP_IP, UDP_PORT))
-        # sock.sendto(bytes([i]) + s[i * 46080:(i + 1) * 46080], (UDP_IP, UDP_PORT))
-        # sock.send(bytes([i]) + s[i * 786431:(i + 1) * 786431])
+        sock.sendto(bytes([i]) + s[i * 19080:(i + 1) * 19080], (TCP_IP, TCP_PORT))
+
         resp = sock.recv(1024) #서버로부터 답신
 
         print(resp.decode())
 
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-    # sock.sendto(value_encrypt, (UDP_IP, UDP_PORT))
+        # if cv2.waitKey(1) & 0xFF == ord('q'):
+        #     break
+
+
+        #-------------------------------
+        
+    pdb.set_trace()
+    # imlength = len(picture)
+    just_value =trans_format_RGB(just_vector[:imlength])
+    pdb.set_trace()
+    picture_size = int (np.sqrt((np.array(just_value).size/3)))
+    just_array = np.array(just_value).reshape(picture_size, picture_size, 3)
+    # opencv_image=cv2.cvtColor(np.array(just_value), cv2.COLOR_RGB2BGR)
+
+    cv2.imwrite("client_tcp_dec_cv2.jpg", just_array)
+    im = Image.new(mode="RGB", size=(picture_size,picture_size))
+    im.putdata(just_value)
+    im.save("client_tcp_dec_pil.jpg")
+    # sock.sendto(value_encrypt, (TCP_IP, TCP_PORT))
 #https://awakening95.tistory.com/1
--- server.py
+++ server.py
@@ -1,17 +1,18 @@
 import time
 import socket
-import numpy
+import numpy as np
 import cv2
 import argparse
 import logging
 from aes import AES
 import pdb
+import base64
 from PIL import Image
 UDP_IP = "127.0.0.1"
 UDP_PORT = 9505
 mode = "ecb"
 
-iteration = 100
+iteration = 1
 print("iteration : {}".format(iteration))
 
 def trans_format_RGB(data):
@@ -36,65 +37,55 @@ aes.key_generation()
 end1 = time.time()
 print(mode + " key gen {} s".format(end1-start1))
 
-org_file = 'Jungkook.png'
-img = cv2.imread(org_file)
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-faces = face_cascade.detectMultiScale(img, 1.3, 5)
-
-im = Image.open(org_file)
-# #Convert image data into pixel value bytes
-value_vector = im.convert("RGB").tobytes()
-
-imlength = len(value_vector)
-# value_vector = frame.convert("RGB").tobytes()
-# 
 conn, addr = sock.accept()
 while True:
     picture = b''
-    
-    # data = conn.recv(786432)
-    # data = conn.recv(46081)
+
     data = conn.recv(19081)
     conn.send('one'.encode())
-    # s[data[0]] = data[1:786432]
-    # s[data[0]] = data[1:46081]
+
     s[data[0]] = data[1:19081]
     print(data[0])
     if data[0] == 19:
         for i in range(20):
             picture += s[i]
 
-        frame = numpy.fromstring(picture, dtype=numpy.uint8)
-    # encrypted_vector = picture
-        just_vector = picture
-    # if mode == "ecb":
-    #     start3 = time.time()
-    #     for i in range(iteration):
-    #         decrypted_vector = aes.aes_ecb_decrypt(aes.pad(encrypted_vector))
-    #     end3 = time.time()
-    # elif mode == "cbc":
-    #     start3 = time.time()
-    #     for i in range(iteration):
-    #         decrypted_vector = aes.aes_cbc_decrypt(encrypted_vector)
-    #     end3 = time.time()
-    # elif mode == "ofb":
-    #     start3 = time.time()
-    #     for i in range(iteration):
-    #         decrypted_vector = aes.aes_ofb_decrypt(encrypted_vector)
-    #     end3 = time.time()
-    # print("key dec {} s".format(end3-start3))
-    # value_decrypt = trans_format_RGB(decrypted_vector[:786432])
-    
-
-    # value_array = numpy.array(value_decrypt).reshape(512, 512, 3)
-        just_value =trans_format_RGB(just_vector)
-
+        frame = np.fromstring(picture, dtype=np.uint8)
+        just_vector = frame
         pdb.set_trace()
-        picture_size = int (numpy.sqrt((numpy.array(just_value).size/3)))
-        just_array = numpy.array(just_value).reshape(picture_size, picture_size, 3)
-        cv2.imwrite("dec_tcp.jpg", just_array)
+        #-------------------------------
+        encrypted_vector = picture
+        if mode == "ecb":
+            start3 = time.time()
+            for i in range(iteration):
+                decrypted_vector = aes.aes_ecb_decrypt(aes.pad(encrypted_vector))
+            end3 = time.time()
+        elif mode == "cbc":
+            start3 = time.time()
+            for i in range(iteration):
+                decrypted_vector = aes.aes_cbc_decrypt(encrypted_vector)
+            end3 = time.time()
+        elif mode == "ofb":
+            start3 = time.time()
+            for i in range(iteration):
+                decrypted_vector = aes.aes_ofb_decrypt(encrypted_vector)
+            end3 = time.time()
+        just_vector = decrypted_vector
+         #-------------------------------
+        print(decrypted_vector)
+        # imlength = len(picture)
+        imlength = 47628
+        just_value =trans_format_RGB(just_vector[:imlength])
+        pdb.set_trace()
+        picture_size = int (np.sqrt((np.array(just_value).size/3)))
+        just_array = np.array(just_value).reshape(picture_size, picture_size, 3)
+        # opencv_image=cv2.cvtColor(np.array(just_value), cv2.COLOR_RGB2BGR)
+
+        cv2.imwrite("get_tcp.jpg", just_array)
+        im = Image.new(mode="RGB", size=(picture_size,picture_size))
+        im.putdata(just_value)
+        im.save("get_tcp_pil.jpg")
         cv2.imshow("frame", just_array)
-    # out.write(frame)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         cv2.destroyAllWindows()
