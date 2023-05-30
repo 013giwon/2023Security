fd1a635016b21ef55f16ca70e257c20ed6ad2617
giwonsur <013giwon@gmail.com>
2023-05-30 16:27:06 +0900

    argparse and jpg

--- client.py
+++ client.py
@@ -3,6 +3,7 @@ import cv2
 import time
 from PIL import Image
 from aes import AES
+import argparse
 import numpy as np
 import logging
 import pdb
@@ -15,146 +16,166 @@ def trans_format_RGB(data):
     red, green, blue = tuple(map(lambda e: [data[i] for i in range(0, len(data)) if i % 3 == e], [0, 1, 2]))
     pixels = tuple(zip(red, green, blue))
     return pixels
-TCP_IP = '127.0.0.1'
-TCP_PORT = 5959
-mode = sys.argv[1]
-bit_error = int(sys.argv[2])
-stride = 21
-patch_size = 21
-pdb.set_trace()
-stride = int(sys.argv[3])
-patch_size = int(sys.argv[4])
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)#socket.SOCK_DGRAM)
-#UDP인경우 생략, TCP는 해주어야 함
-sock.connect((TCP_IP, TCP_PORT))
+
+def main(args):
+    TCP_IP = args.addr
+    TCP_PORT = int(args.port)
+    mode = args.mode
+    bit_error = int(args.bit_error) 
+    stride = int(args.stride)
+    patch_size = int(args.patch_size)
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)#socket.SOCK_DGRAM)
+    #UDP인경우 생략, TCP는 해주어야 함
+    sock.connect((TCP_IP, TCP_PORT))
 
 
-face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-org_file = 'Jungkook.jpg'
-img = cv2.imread(org_file)
-# img = Image.open(org_file)
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-faces = face_cascade.detectMultiScale(img, 1.3, 5)
-aes = AES("aes", 256)
-iteration = 1
-start1 = time.time()
+    face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+    org_file = args.file_name
+    img = cv2.imread(org_file)
+    # img = Image.open(org_file)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(img, 1.3, 5)
+    aes = AES("aes", 256)
+    iteration = 1000
+    start1 = time.time()
 
-aes.key_generation()
-for (x,y,w,h) in faces:
-    img = cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
-    roi_color = img[y:y+h, x:x+w]
-pdb.set_trace()
-cv2.imwrite("target_face.jpg", roi_color)
+    aes.key_generation()
+    for (x,y,w,h) in faces:
+        img = cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+        roi_color = img[y:y+h, x:x+w]
 
-patch = Patches()
+    cv2.imwrite("target_face.jpg", roi_color)
 
-for f in range (1):
-    # ret, frame = cap.read()
-    time.sleep(2)
-    # get image
-    filename = "target_face.jpg"
-    original_img = Image.open(filename)
-    
-    # patch generation
-    img_patched, img_num_pos= patch.patch_generation(original_img, stride, patch_size) 
+    patch = Patches()
 
-    pdb.set_trace() # img_num_pos check
-
-    picture_size = int (np.sqrt((np.array(img_patched).size/3)))
-    im = trans_format_RGB(img_patched.flatten())
-    im2 = Image.new(mode="RGB", size=(picture_size,picture_size))
-    im2.putdata(im)
-    im2.save("client_img_patched_" + str(patch_size) + ".jpg") 
-    value_vector_o = img_patched.tobytes() # encrypted patch images
-    pdb.set_trace()
-
-    random_bytes = random.randint(2,len(value_vector_o))
-    if bit_error == True:
-        if value_vector_o[random_bytes]%2 == 0:
-            replace_num = value_vector_o[random_bytes] + 1
-        else:
-            replace_num = value_vector_o[random_bytes] -1
-        print(bin(value_vector_o[random_bytes]))
-        print(bin(replace_num))
-        value_vector = value_vector_o[0:random_bytes]  + bytes([replace_num]) + value_vector_o[random_bytes+1:]
-    else:
-        value_vector = value_vector_o
-
-    imlength = len(value_vector)
-
-    # image encryption
-    if mode == "ecb":
-        start2 = time.time()
-        for i in range(iteration):
-            encrypted_vector = aes.aes_ecb_encrypt(aes.pad(value_vector))
-        end2 = time.time()
-    elif mode == "cbc":
-        start2 = time.time()
-        for i in range(iteration):
-            encrypted_vector = aes.aes_cbc_encrypt(aes.pad(value_vector))
-        end2 = time.time()
-    elif mode == "ofb":
-        start2 = time.time()
-        for i in range(iteration):
-            encrypted_vector = aes.aes_ofb_encrypt(aes.pad(value_vector))
-        end2 = time.time()
-    elif mode == "cfb":
-        start2 = time.time()
-        for i in range(iteration):
-            encrypted_vector = aes.aes_cfb_encrypt(aes.pad(value_vector))
-        end2 = time.time()
+    for f in range (1):
+        # ret, frame = cap.read()
+        time.sleep(2)
+        # get image
+        filename = "target_face.jpg"
+        original_img = Image.open(filename)
         
-    print("enc time picture {}".format(end2-start2))
-    value_encrypt = np.array(encrypted_vector)
-    d = value_encrypt.flatten()
-    s = d.tostring()
+        # patch generation
+        img_patched, img_num_pos= patch.patch_generation(original_img, stride, patch_size) 
+
+    # pdb.set_trace() # img_num_pos check
+
+        picture_size = int (np.sqrt((np.array(img_patched).size/3)))
+        im = trans_format_RGB(img_patched.flatten())
+        im2 = Image.new(mode="RGB", size=(picture_size,picture_size))
+        im2.putdata(im)
+        im2.save("client_img_patched_" + str(patch_size) + ".jpg") 
+        value_vector_o = img_patched.tobytes() # encrypted patch images
+    # pdb.set_trace()
+
+        random_bytes = random.randint(2,len(value_vector_o))
+
+        if bit_error == True:
+            if value_vector_o[random_bytes]%2 == 0:
+                replace_num = value_vector_o[random_bytes] + 1
+            else:
+                replace_num = value_vector_o[random_bytes] -1
+            print(bin(value_vector_o[random_bytes]))
+            print(bin(replace_num))
+            value_vector = value_vector_o[0:random_bytes]  + bytes([replace_num]) + value_vector_o[random_bytes+1:]
+        else:
+            value_vector = value_vector_o
+
+        imlength = len(value_vector)
+
+        # image encryption
+        if mode == "ecb":
+            start2 = time.time()
+            for i in range(iteration):
+                encrypted_vector = aes.aes_ecb_encrypt(aes.pad(value_vector))
+            end2 = time.time()
+        elif mode == "cbc":
+            start2 = time.time()
+            for i in range(iteration):
+                encrypted_vector = aes.aes_cbc_encrypt(aes.pad(value_vector))
+            end2 = time.time()
+        elif mode == "ofb":
+            start2 = time.time()
+            for i in range(iteration):
+                encrypted_vector = aes.aes_ofb_encrypt(aes.pad(value_vector))
+            end2 = time.time()
+        elif mode == "cfb":
+            start2 = time.time()
+            for i in range(iteration):
+                encrypted_vector = aes.aes_cfb_encrypt(aes.pad(value_vector))
+            end2 = time.time()
+            
+        print("enc time picture {}".format(end2-start2))
+        value_encrypt = np.array(encrypted_vector)
+        d = value_encrypt.flatten()
+        s = d.tostring()
 
 
-    encrypted_vector = s
-    shuffle_key = [imlength, np.array(original_img).shape[0], np.array(original_img).shape[1], stride, patch_size]
-    for i in img_num_pos:
-        shuffle_key.append(i[2])
-    pdb.set_trace()
+        encrypted_vector = s
+        shuffle_key = [imlength, np.array(original_img).shape[0], np.array(original_img).shape[1], stride, patch_size]
+        for i in img_num_pos:
+            shuffle_key.append(i[2])
+        pdb.set_trace()
 
-    shuff_key_byte = bytes(str(shuffle_key), 'utf-8')
- 
-    # key encryption
-    if mode == "ecb":
-        start2 = time.time()
-        for i in range(iteration):
-            encrypted_vector2 = aes.aes_ecb_encrypt(aes.pad(shuff_key_byte))
-        end2 = time.time()
-    elif mode == "cbc":
-        start2 = time.time()
-        for i in range(iteration):
-            encrypted_vector2 = aes.aes_cbc_encrypt(aes.pad(shuff_key_byte))
-        end2 = time.time()
-    elif mode == "ofb":
-        start2 = time.time()
-        for i in range(iteration):
-            encrypted_vector2 = aes.aes_ofb_encrypt(aes.pad(shuff_key_byte))
-        end2 = time.time()
-    elif mode == "cfb":
-        start2 = time.time()
-        for i in range(iteration):
-            encrypted_vector2 = aes.aes_cfb_encrypt(aes.pad(shuff_key_byte))
-        end2 = time.time()
-    print("enc time key {}".format(end2-start2))
-    print('=======key in client=======')
-    print(shuff_key_byte)
+        shuff_key_byte = bytes(str(shuffle_key), 'utf-8')
     
-    for i in range(20):
-        sock.sendto(bytes([i]) + s[i * 19080:(i + 1) * 19080], (TCP_IP, TCP_PORT))
-        resp = sock.recv(1024) #서버로부터 답신
-        print(resp.decode())
+        # key encryption
+        if mode == "ecb":
+            start2 = time.time()
+            for i in range(iteration):
+                encrypted_vector2 = aes.aes_ecb_encrypt(aes.pad(shuff_key_byte))
+            end2 = time.time()
+        elif mode == "cbc":
+            start2 = time.time()
+            for i in range(iteration):
+                encrypted_vector2 = aes.aes_cbc_encrypt(aes.pad(shuff_key_byte))
+            end2 = time.time()
+        elif mode == "ofb":
+            start2 = time.time()
+            for i in range(iteration):
+                encrypted_vector2 = aes.aes_ofb_encrypt(aes.pad(shuff_key_byte))
+            end2 = time.time()
+        elif mode == "cfb":
+            start2 = time.time()
+            for i in range(iteration):
+                encrypted_vector2 = aes.aes_cfb_encrypt(aes.pad(shuff_key_byte))
+            end2 = time.time()
 
-    value_encrypt2 = np.array(encrypted_vector2)
-    d_key = value_encrypt2.flatten()
-    s_key = d_key.tostring()
-    if resp.decode() == "please send the shuffle key":
-        print("key send now!")
+        print("enc time key {}".format(end2-start2))
+        print('=======key in client=======')
+        print(shuff_key_byte)
+        
+        for i in range(20):
+            sock.sendto(bytes([i]) + s[i * 19080:(i + 1) * 19080], (TCP_IP, TCP_PORT))
+            resp = sock.recv(1024) #서버로부터 답신
+            print(resp.decode())
 
-        sock.sendto(bytes([20]) + s_key , (TCP_IP, TCP_PORT))
-        #-------------------------------
+        value_encrypt2 = np.array(encrypted_vector2)
+        d_key = value_encrypt2.flatten()
+        s_key = d_key.tostring()
+        if resp.decode() == "please send the shuffle key":
+            print("key send now!")
+
+            sock.sendto(bytes([20]) + s_key , (TCP_IP, TCP_PORT))
+            #-------------------------------
         
 #https://awakening95.tistory.com/1
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-m', '--mode', type=str, default='ecb',
+                        help='AES ecb/cbc/cfb/ofb.')
+    parser.add_argument('-e', '--bit_error', type=str, default='0',
+                        help=' 0 : normal 1 :one-bit error')
+    parser.add_argument('-p', '--port', type=str, default='5959',
+                        help='TCP port.')
+    parser.add_argument('-a', '--addr', type=str, default='127.0.0.1',
+                        help='TCP addr.')
+    parser.add_argument('-s', '--stride', type=str, default='21',
+                        help='patch stride.')
+    parser.add_argument('-ps', '--patch_size', type=str, default='21',
+                        help='patch_size.')
+    parser.add_argument('-f', '--file_name', type=str, default='',
+                        help='file_name.')
+    args = parser.parse_args()
+    main(args)
+    #python client.py -m ecb -e 0 --port 5959 --addr 127.0.0.1 -f Jungkook.jpg
--- server.py
+++ server.py
@@ -11,165 +11,175 @@ from PIL import Image
 from patches import Patches
 import sys
 import random
-TCP_IP = "127.0.0.1"
-TCP_PORT = 5959
-mode = sys.argv[1]
-bit_error = int(sys.argv[2])
-iteration = 1
-print("iteration : {}".format(iteration))
+
+
 
 def trans_format_RGB(data):
     #tuple: Immutable, ensure that data is not lost
     red, green, blue = tuple(map(lambda e: [data[i] for i in range(0, len(data)) if i % 3 == e], [0, 1, 2]))
     pixels = tuple(zip(red, green, blue))
     return pixels
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM ) #socket.SOCK_DGRAM)
-sock.bind((TCP_IP, TCP_PORT))
-sock.listen(True)
 
-# s = [b'\xff' * 46080 for x in range(20)]
-s = [b'\xff' * 19080 for x in range(20)]
-key_byte = [b'\xff' * 19080 for x in range(21)]
-face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
-aes = AES("aes", 256)
-start1 = time.time()
-
-aes.key_generation()
-
-end1 = time.time()
-print(mode + " key gen {} s".format(end1-start1))
-
-conn, addr = sock.accept()
-while True:
-    picture = b''
-    data = conn.recv(19081)
-    
-    if data[0] < 19:
-        conn.send(str(data[0]).encode())
-    key_byte[data[0]] = data[1:19081]
-    
-    if data[0] == 19:
-        for i in range(20):
-            picture += key_byte[i]
-
-        frame = np.fromstring(picture, dtype=np.uint8)
-        just_vector = frame
-        conn.send('please send the shuffle key'.encode())
-
-        # image decryption
-        value_vector_o = picture
+def main(args):
+    pdb.set_trace()
+    TCP_IP = args.addr
+    TCP_PORT = int(args.port)
+    mode = args.mode
+    bit_error = int(args.bit_error) 
+    iteration = 1000
+    print("iteration : {}".format(iteration))
 
 
-        random_bytes = random.randint(2,len(value_vector_o))
-        if bit_error == True:
-            if value_vector_o[random_bytes]%2 == 0:
-                replace_num = value_vector_o[random_bytes] + 1
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM ) #socket.SOCK_DGRAM)
+    sock.bind((TCP_IP, TCP_PORT))
+    sock.listen(True)
+
+    s = [b'\xff' * 19080 for x in range(20)]
+    key_byte = [b'\xff' * 19080 for x in range(21)]
+
+    aes = AES("aes", 256)
+    start1 = time.time()
+
+    aes.key_generation()
+
+    end1 = time.time()
+    # print(mode + " key gen {} s".format(end1-start1))
+
+    conn, addr = sock.accept()
+    while True:
+        picture = b''
+        data = conn.recv(19081)
+        
+        if data[0] < 19: 
+            conn.send(str(data[0]).encode())
+        key_byte[data[0]] = data[1:19081]
+        
+        if data[0] == 19:
+            for i in range(20):
+                picture += key_byte[i]
+
+            frame = np.fromstring(picture, dtype=np.uint8)
+            just_vector = frame
+            conn.send('please send the shuffle key'.encode())
+
+            # image decryption
+            value_vector_o = picture
+
+            random_bytes = random.randint(2,len(value_vector_o))
+            if bit_error == True:
+                if value_vector_o[random_bytes]%2 == 0:
+                    replace_num = value_vector_o[random_bytes] + 1
+                else:
+                    replace_num = value_vector_o[random_bytes] -1
+                print(bin(value_vector_o[random_bytes]))
+                print(bin(replace_num))
+                encrypted_vector = value_vector_o[0:random_bytes]  + bytes([replace_num]) + value_vector_o[random_bytes+1:]
             else:
-                replace_num = value_vector_o[random_bytes] -1
-            print(bin(value_vector_o[random_bytes]))
-            print(bin(replace_num))
-            encrypted_vector = value_vector_o[0:random_bytes]  + bytes([replace_num]) + value_vector_o[random_bytes+1:]
-        else:
-            encrypted_vector = value_vector_o
-        if mode == "ecb":
-            start3 = time.time()
-            for i in range(iteration):
-                decrypted_vector = aes.aes_ecb_decrypt(aes.pad(encrypted_vector))
-            end3 = time.time()
-        elif mode == "cbc":
-            start3 = time.time()
-            for i in range(iteration):
-                decrypted_vector = aes.aes_cbc_decrypt(aes.pad(encrypted_vector))
-            end3 = time.time()
-        elif mode == "ofb":
-            start3 = time.time()
-            for i in range(iteration):
-                decrypted_vector = aes.aes_ofb_decrypt(aes.pad(encrypted_vector))
-            end3 = time.time()
-        elif mode == "cfb":
-            start3 = time.time()
-            for i in range(iteration):
-                decrypted_vector = aes.aes_cfb_decrypt(aes.pad(encrypted_vector))
-            end3 = time.time()
-        elif mode == "ctr":
-            start3 = time.time()
-            for i in range(iteration):
-                decrypted_vector = aes.aes_ctr_decrypt(aes.pad(encrypted_vector))
-            end3 = time.time()
-        print("dec time picture {}".format(end3-start3))
-        just_vector = decrypted_vector
+                encrypted_vector = value_vector_o
+
+            if mode == "ecb":
+                start3 = time.time()
+                for i in range(iteration):
+                    decrypted_vector = aes.aes_ecb_decrypt(aes.pad(encrypted_vector))
+                end3 = time.time()
+            elif mode == "cbc":
+                start3 = time.time()
+                for i in range(iteration):
+                    decrypted_vector = aes.aes_cbc_decrypt(aes.pad(encrypted_vector))
+                end3 = time.time()
+            elif mode == "ofb":
+                start3 = time.time()
+                for i in range(iteration):
+                    decrypted_vector = aes.aes_ofb_decrypt(aes.pad(encrypted_vector))
+                end3 = time.time()
+            elif mode == "cfb":
+                start3 = time.time()
+                for i in range(iteration):
+                    decrypted_vector = aes.aes_cfb_decrypt(aes.pad(encrypted_vector))
+                end3 = time.time()
+
+            print("dec time picture {}".format(end3-start3))
+            just_vector = decrypted_vector
+            
+            
+        # shuffle key decryption
+        if data[0] == 20:
+            key = key_byte[data[0]]
+            key_len = data[-1]
+
+            encrypted_vector = key #key[:10]
+            if mode == "ecb":
+                start3 = time.time()
+                for i in range(iteration):
         
-         #-------------------------------
+                    decrypted_vector = aes.aes_ecb_decrypt(aes.pad(encrypted_vector))
+                end3 = time.time()
+            elif mode == "cbc":
+                start3 = time.time()
+                for i in range(iteration):
+                    decrypted_vector = aes.aes_cbc_decrypt(aes.pad(encrypted_vector))
+                end3 = time.time()
+            elif mode == "ofb":
+                start3 = time.time()
+                for i in range(iteration):
+                    decrypted_vector = aes.aes_ofb_decrypt(aes.pad(encrypted_vector))
+                end3 = time.time()
+            elif mode == "cfb":
+                start3 = time.time()
+                for i in range(iteration):
+                    decrypted_vector = aes.aes_cfb_decrypt(aes.pad(encrypted_vector))
+                end3 = time.time()
 
-        
-        # cv2.imshow("frame", just_array)
-        
-    # shuffle key decryption
-    if data[0] == 20:
-        key = key_byte[data[0]]
-        key_len = data[-1]
-        
-        # key = np.fromstring(key, dtype=np.uint8)
+            print("dec time key {}".format(end3-start3))
+            pdb.set_trace()
+            dec_shuffle_key = decrypted_vector.decode('utf-8')
 
-        encrypted_vector = key #key[:10]
-        if mode == "ecb":
-            start3 = time.time()
-            for i in range(iteration):
-    
-                decrypted_vector = aes.aes_ecb_decrypt(aes.pad(encrypted_vector))
-            end3 = time.time()
-        elif mode == "cbc":
-            start3 = time.time()
-            for i in range(iteration):
-                decrypted_vector = aes.aes_cbc_decrypt(aes.pad(encrypted_vector))
-            end3 = time.time()
-        elif mode == "ofb":
-            start3 = time.time()
-            for i in range(iteration):
-                decrypted_vector = aes.aes_ofb_decrypt(aes.pad(encrypted_vector))
-            end3 = time.time()
-        elif mode == "cfb":
-            start3 = time.time()
-            for i in range(iteration):
-                decrypted_vector = aes.aes_cfb_decrypt(aes.pad(encrypted_vector))
-            end3 = time.time()
-        print("dec time key{}".format(end3-start3))
-        pdb.set_trace()
-        dec_shuffle_key = decrypted_vector.decode('utf-8')
-        # dec_shuffle_key = decrypted_vector[:key_len].decode('utf-8')
-        dec_shuff_k = dec_shuffle_key[1:-1].split(',')
-        
-        img_size = int(dec_shuff_k[0])
-        img_h = int(dec_shuff_k[1])
-        img_w = int(dec_shuff_k[2])
-        patch_size = int(dec_shuff_k[4])
-        shuff_key_list = [] 
-        total =  (img_h // (patch_size)) * (img_w // (patch_size))
-        for i in range(total + 5 - 1):
-            shuff_key_list.append(int(dec_shuff_k[i].strip()))
-        pdb.set_trace()
-        shuff_key_list.append(int(dec_shuff_k[5+total-1].split(']')[0]))
+            dec_shuff_k = dec_shuffle_key[1:-1].split(',')
+            
+            img_size = int(dec_shuff_k[0])
+            img_h = int(dec_shuff_k[1])
+            img_w = int(dec_shuff_k[2])
+            patch_size = int(dec_shuff_k[4])
+            shuff_key_list = [] 
+            total =  (img_h // (patch_size)) * (img_w // (patch_size))
+            for i in range(total + 5 - 1):
+                shuff_key_list.append(int(dec_shuff_k[i].strip()))
+            pdb.set_trace()
+            shuff_key_list.append(int(dec_shuff_k[5+total-1].split(']')[0]))
 
-        imlength = shuff_key_list[0]
-        just_value =trans_format_RGB(just_vector[:imlength])
-        picture_size = int (np.sqrt((np.array(just_value).size/3)))
-        just_array = np.array(just_value).reshape(picture_size, picture_size, 3)
+            imlength = shuff_key_list[0]
+            just_value =trans_format_RGB(just_vector[:imlength])
+            picture_size = int (np.sqrt((np.array(just_value).size/3)))
+            just_array = np.array(just_value).reshape(picture_size, picture_size, 3)
 
-        im = Image.new(mode="RGB", size=(picture_size,picture_size))
-        im.putdata(just_value)
-        im.save("get_tcp_pil.jpg")      
-        # patches reconstruction 
-        patch = Patches()
-        img = patch.patch_reconst(shuff_key_list[3:], just_array) 
-        pdb.set_trace()    
+            im = Image.new(mode="RGB", size=(picture_size,picture_size))
+            im.putdata(just_value)
+            im.save("get_tcp_shuffled.jpg")      
+            # patches reconstruction 
+            patch = Patches()
+            img = patch.patch_reconst(shuff_key_list[3:], just_array) 
+            pdb.set_trace()    
 
-        rec_value =trans_format_RGB(img.flatten())
-        im2 = Image.new(mode="RGB", size=(picture_size,picture_size))
-        im2.putdata(rec_value)
-        im2.save("pil_tcp_reconstructed_" + mode + ".jpg") 
+            rec_value =trans_format_RGB(img.flatten())
+            im2 = Image.new(mode="RGB", size=(picture_size,picture_size))
+            im2.putdata(rec_value)
+            im2.save("pil_tcp_reconstructed_" + mode + ".jpg") 
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        cv2.destroyAllWindows()
-        break
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            cv2.destroyAllWindows()
+            break
 
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-m', '--mode', type=str, default='ecb',
+                        help='AES ecb/cbc/cfb/ofb.')
+    parser.add_argument('-e', '--bit_error', type=str, default='0',
+                        help=' 0 : normal 1 :one-bit error')
+    parser.add_argument('-p', '--port', type=str, default='5959',
+                        help='TCP port.')
+    parser.add_argument('-a', '--addr', type=str, default='127.0.0.1',
+                        help='TCP addr.')
+    args = parser.parse_args()
+    main(args)
+
+    #python server.py -m ecb -p 5959 -e 0
